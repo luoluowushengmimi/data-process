7cc9e54f00775419a092e2dd04f5b1fbae5919f9
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-07-18 16:28:26 +0800

    add regex to the class

--- src/cn/cnic/virostudio/process/PreProcessor.java
+++ src/cn/cnic/virostudio/process/PreProcessor.java
@@ -1,6 +1,10 @@
 package cn.cnic.virostudio.process;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.log4j.Logger;
 import cn.cnic.virostudio.rule.FilterCondition;
 import com.google.common.collect.ArrayListMultimap;
@@ -18,19 +22,6 @@ public class PreProcessor {
 	public void setConditions(List<FilterCondition> conditions) {
 		this.conditions = conditions;
 	}
-	/**
-	 * 预处理，从BindingSet获取map
-	 */
-//	public Multimap<String, String> getMap(BindingSet set) {
-//		
-//		Multimap<String, String> result = ArrayListMultimap.create();
-//		for(String key : set.getBindingNames()) {
-//			result.put(key, set.getValue(key).stringValue().replace("\"", ""));
-//		}
-//		loginfo.info("map: "+result);
-//		return this.getFilterResult(result);
-//		//return result;
-//	}
 	
 	public Multimap<String, String> getFilterResult(Multimap<String, String> map){
 		if(conditions==null||conditions.size()==0)
@@ -64,69 +55,11 @@ public class PreProcessor {
 	public boolean isSave(Multimap<String, String> map,FilterCondition condition){
 		boolean tag=false;
 		if(!map.containsKey(condition.getKey())) return tag;
-		//info.info("一个留下的条件的初始tag：" +tag);
 		String mapValue=map.get(condition.getKey()).iterator().next().trim();
-		//info.info("mapValue:" +mapValue);
-		String conditionValue=condition.getValue().trim();
-		//info.info("conditionValue:" +conditionValue);
-		if(condition.getMatchRule().equalsIgnoreCase("start")&&!conditionValue.contains(",")){
-			//info.info("condition 进入start and without ,:" +conditionValue);
-			tag=this.isStartSave(mapValue, conditionValue);
-			//info.info(" 进入start and without , " +tag);
-		}
-		else if(condition.getMatchRule().equalsIgnoreCase("equal")&&!conditionValue.contains(",")){
-			//info.info("condition 进入equal and without ,:" +conditionValue);
-			tag=this.isEqualSave(mapValue, conditionValue);
-			//info.info(" 进入equal and without , 的tag 是: " +tag);
-		}
-		else if(condition.getMatchRule().equalsIgnoreCase("start")&&conditionValue.contains(",")){
-			//info.info("condition 进入start and with |:" +conditionValue);
-			tag=this.isStartSave(mapValue, conditionValue.split(","));
-			//info.info(" 进入start and with , 的tag 是: " +tag);
-		}
-		else if(condition.getMatchRule().equalsIgnoreCase("equal")&&conditionValue.contains(",")){
-			//info.info("condition 进入equal and with ," +conditionValue);
-			tag=this.isEqualSave(mapValue, conditionValue.split(","));
-			//info.info(" 进入equal and with , 的tag 是: " +tag);
-		}
-		return tag;
+		String regEx=condition.getValue().trim();
+		Pattern pattern=Pattern.compile(regEx);
+		Matcher matcher=pattern.matcher(mapValue);
+		return pattern.matches(regEx, mapValue);
 		
 	}
-	public boolean isStartSave(String mapValue,String[] conditionValue){
-		boolean tag=false;
-		for(String con:conditionValue){
-			//info.info("con: "+con);
-			//info.info("this.isStartSave: "+this.isStartSave(mapValue, con));
-			if(this.isStartSave(mapValue, con)){
-				tag=true;
-			}
-		}
-		return tag;
-	}
-	public boolean isEqualSave(String mapValue,String[] conditionValue){
-		boolean tag=false;
-		for(String con:conditionValue){
-			if(this.isStartSave(mapValue, con)){
-				tag=true;
-			}
-		}
-		return tag;
-	}
-	
-	public boolean isStartSave(String mapValue,String conditionValue){
-		boolean tag=false;
-		if(mapValue.startsWith(conditionValue)){
-			tag=true;
-		}
-		return tag;
-	}
-	public boolean isEqualSave(String mapValue,String conditionValue){
-		boolean tag=false;
-		if(mapValue.equalsIgnoreCase(conditionValue)){
-			tag=true;
-		}
-		return tag;
-	}
-	
-	
 }
