f7741de552d3b63828585c825c42803de9398bcf
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-07-18 16:57:17 +0800

    genebank rukou

---
+++ src/cn/cnic/virostudio/StartGenebank.java
@@ -0,0 +1,21 @@
+package cn.cnic.virostudio;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.TreeMultimap;
+
+import cn.cnic.virostudio.job.JobController;
+import cn.cnic.virostudio.process.CompositeProcessor;
+public class StartGenebank {
+	private static Logger logerr = Logger.getLogger("errLog");
+	private static Logger loginfo = Logger.getLogger("infoLog");
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "genebank-job.xml" });
+		JobController job = context.getBean("job", JobController.class);
+		job.doListener();
+	}
+
+}
