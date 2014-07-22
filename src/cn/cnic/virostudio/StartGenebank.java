153183b51235786730c036df14e00f0ce1dde768
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-07-22 10:50:08 +0800

    ok

--- src/cn/cnic/virostudio/StartGenebank.java
+++ src/cn/cnic/virostudio/StartGenebank.java
@@ -3,11 +3,7 @@ package cn.cnic.virostudio;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.TreeMultimap;
-
 import cn.cnic.virostudio.job.JobController;
-import cn.cnic.virostudio.process.CompositeProcessor;
 public class StartGenebank {
 	private static Logger logerr = Logger.getLogger("errLog");
 	private static Logger loginfo = Logger.getLogger("infoLog");
