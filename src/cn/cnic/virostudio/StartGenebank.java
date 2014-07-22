0b4910ff3546da54bd9753c0a8596aa98323406e
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-07-22 10:55:46 +0800

    ClassPathXmlApplicationContext close

--- src/cn/cnic/virostudio/StartGenebank.java
+++ src/cn/cnic/virostudio/StartGenebank.java
@@ -1,16 +1,14 @@
 package cn.cnic.virostudio;
 
-import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import cn.cnic.virostudio.job.JobController;
 public class StartGenebank {
-	private static Logger logerr = Logger.getLogger("errLog");
-	private static Logger loginfo = Logger.getLogger("infoLog");
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				new String[] { "genebank-job.xml" });
 		JobController job = context.getBean("job", JobController.class);
 		job.doListener();
+		context.close();
 	}
 }
