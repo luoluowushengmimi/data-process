e8d441d81c227287e22275a4beadbbd04781ad33
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-07-28 14:18:39 +0800

    need to test

--- src/cn/cnic/virostudio/StartGene.java
+++ src/cn/cnic/virostudio/StartGene.java
@@ -2,20 +2,19 @@ package cn.cnic.virostudio;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import cn.cnic.virostudio.job.JobController;
 
 public class StartGene {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext(
-				new String[] { "gene-job1.xml" });
-		JobController job1 = context1.getBean("job", JobController.class);
-		job1.doListener();
-		context1.close();
-		ClassPathXmlApplicationContext context2 = new ClassPathXmlApplicationContext(
-				new String[] { "gene-job2.xml" });
-		JobController job2 = context2.getBean("job", JobController.class);
-		job2.doListener();
-		context2.close();
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "gene-job-one.xml" });
+		JobController job = context.getBean("job", JobController.class);
+		job.doListener();
+		context.close();
+//		ClassPathXmlApplicationContext contextnew = new ClassPathXmlApplicationContext(
+//				new String[] { "gene-job2.xml" });
+//		JobController job2 = context2.getBean("job", JobController.class);
+//		job2.doListener();
+//		context2.close();
 	}
 }
