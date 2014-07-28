5aa4b99d80ef735664c7d50da64ccaefd2ddf445
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-07-28 13:48:25 +0800

    gene 处理的两个部分

--- src/cn/cnic/virostudio/StartGene.java
+++ src/cn/cnic/virostudio/StartGene.java
@@ -7,10 +7,15 @@ import cn.cnic.virostudio.job.JobController;
 
 public class StartGene {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[] { "gene-job.xml" });
-		JobController job = context.getBean("job", JobController.class);
-		job.doListener();
-		context.close();
+		ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext(
+				new String[] { "gene-job1.xml" });
+		JobController job1 = context1.getBean("job", JobController.class);
+		job1.doListener();
+		context1.close();
+		ClassPathXmlApplicationContext context2 = new ClassPathXmlApplicationContext(
+				new String[] { "gene-job2.xml" });
+		JobController job2 = context2.getBean("job", JobController.class);
+		job2.doListener();
+		context2.close();
 	}
 }
