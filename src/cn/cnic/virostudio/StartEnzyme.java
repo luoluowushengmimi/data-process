fac189e544d4cfb3450ac9c2f417d9a41ed68423
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-08-06 15:44:09 +0800

    ok

--- src/cn/cnic/virostudio/StartEnzyme.java
+++ src/cn/cnic/virostudio/StartEnzyme.java
@@ -2,14 +2,13 @@ package cn.cnic.virostudio;
 
 import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import cn.cnic.virostudio.job.JobController;
 import cn.cnic.virostudio.job.Step;
 
 public class StartEnzyme {
 	private static Logger logerr = Logger.getLogger("errLog");
 	private static Logger loginfo = Logger.getLogger("infoLog");
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 //		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 //				new String[] { "enzyme-job.xml" });
 //		JobController job = context.getBean("job", JobController.class);
