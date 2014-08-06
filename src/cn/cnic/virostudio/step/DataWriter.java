4441a6018e4482d8d738c1402d6f0445ca164486
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-08-06 14:11:25 +0800

    ok

--- src/cn/cnic/virostudio/step/DataWriter.java
+++ src/cn/cnic/virostudio/step/DataWriter.java
@@ -1,5 +1,10 @@
 package cn.cnic.virostudio.step;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.cnic.virostudio.triple.Triple;
+
 import com.google.common.collect.Multimap;
 
 public interface DataWriter {
@@ -8,6 +13,6 @@ public interface DataWriter {
 	public String getIdname();
 	
 	public String getFilePath() ;
-
-
+	
+	public void write(int fileId,ArrayList<Multimap<String, String>> map) throws Exception;
 }
