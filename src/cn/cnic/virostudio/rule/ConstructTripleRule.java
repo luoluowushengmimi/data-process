4085283abf4b614ab246bd15fa085be59ac2b756
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-08-06 14:11:07 +0800

    need to test

---
+++ src/cn/cnic/virostudio/rule/ConstructTripleRule.java
@@ -0,0 +1,35 @@
+package cn.cnic.virostudio.rule;
+
+import java.util.List;
+import java.util.Map;
+
+public class ConstructTripleRule {
+	private List<String> keys;
+	private String sprefix;
+	private String idname;
+
+	public List<String> getKeys() {
+		return keys;
+	}
+
+	public void setKey(List<String> keys) {
+		this.keys = keys;
+	}
+
+	public String getSprefix() {
+		return sprefix;
+	}
+
+	public void setSprefix(String sprefix) {
+		this.sprefix = sprefix;
+	}
+
+	public String getIdname() {
+		return idname;
+	}
+
+	public void setIdname(String idname) {
+		this.idname = idname;
+	}
+
+}
