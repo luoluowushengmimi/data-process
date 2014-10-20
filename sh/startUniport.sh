a6f5d9ea1419eca2071615062c44de9c027e27fe
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-10-20 14:21:47 +0800

    add startuniprot sh

---
+++ sh/startUniport.sh
@@ -0,0 +1,16 @@
+PNAME=virotest
+ENV=prod
+
+echo "starting ${PNAME}"
+
+pid=`ps -ef | grep java | grep "${PNAME}" | awk '{print $2}'`
+
+if [ "a$pid" = "a" ]; then 
+	nohup java -Denv=${ENV} -Dn=${PNAME} -Xmx4000m  -cp ./:./conf/:./lib/* cn.cnic.virostudio.StartUniprot >stdout 2>stderr &
+	echo "started ${PNAME} Entire."
+	exit 0
+fi
+
+echo "ERROR: has been started:$pid"
+exit 1
+
