c7d8621a1d0d19ede970a3b3ba5b0b0d1e3731bf
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-10-22 10:55:25 +0800

    add title

---
+++ src/cn/cnic/virostudio/StartAddTitle.java
@@ -0,0 +1,24 @@
+package cn.cnic.virostudio;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import cn.cnic.virostudio.job.Step;
+
+public class StartAddTitle {
+
+	private static Logger logerr = Logger.getLogger("errLog");
+	private static Logger loginfo = Logger.getLogger("infoLog");
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "add-reaction-title.xml" });
+		Step step = context.getBean("step", Step.class);
+		step.doStep(0);
+		context.close();
+	}
+
+}
