9a9985f799a71bf3ae14f02fe90ff598022e3fca
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-10-30 09:58:58 +0800

    change taxonomy start enter to step class

--- src/cn/cnic/virostudio/Start.java
+++ src/cn/cnic/virostudio/Start.java
@@ -3,15 +3,17 @@ package cn.cnic.virostudio;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import cn.cnic.virostudio.job.JobController;
+import cn.cnic.virostudio.job.Step;
 
 
 
 public class Start {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				new String[] { "taxonomy-job.xml" });
-		JobController job = context.getBean("job", JobController.class);
-		job.doListener();
+		Step step = context.getBean("step", Step.class);
+		step.doStep(0);
+		context.close();
 	}
 }
