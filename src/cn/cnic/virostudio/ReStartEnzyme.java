3523800427963f1775ab99398140680d130e0be1
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-11-06 13:23:22 +0800

    add restart class

---
+++ src/cn/cnic/virostudio/ReStartEnzyme.java
@@ -0,0 +1,24 @@
+package cn.cnic.virostudio;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import cn.cnic.virostudio.job.JobController;
+
+public class ReStartEnzyme {
+
+	private static Logger logerr = Logger.getLogger("errLog");
+	private static Logger loginfo = Logger.getLogger("infoLog");
+	public static void main(String[] args) throws Exception {
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//				new String[] { "enzyme-job.xml" });
+//		JobController job = context.getBean("job", JobController.class);
+//		job.doListener();
+//		context.close();
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "enzyme-job.xml" });
+		JobController job = context.getBean("job", JobController.class);
+		job.doListener();
+		context.close();
+	}
+
+}
---
+++ src/cn/cnic/virostudio/ReStartPathway.java
@@ -0,0 +1,25 @@
+package cn.cnic.virostudio;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.cnic.virostudio.job.JobController;
+
+public class ReStartPathway {
+
+	private static Logger logerr = Logger.getLogger("errLog");
+	private static Logger loginfo = Logger.getLogger("infoLog");
+	public static void main(String[] args) throws Exception {
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//				new String[] { "enzyme-job.xml" });
+//		JobController job = context.getBean("job", JobController.class);
+//		job.doListener();
+//		context.close();
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "pathway-job.xml" });
+		JobController job = context.getBean("job", JobController.class);
+		job.doListener();
+		context.close();
+	}
+
+}
