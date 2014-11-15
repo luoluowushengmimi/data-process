6e5614868387a1b804e9d96ca1b32d0845f2d2a9
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-11-15 17:29:53 +0800

    add 大数据量时的重新程序

--- src/cn/cnic/virostudio/ReStartPathway.java
+++ src/cn/cnic/virostudio/ReStartPathway.java
@@ -4,21 +4,21 @@ import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import cn.cnic.virostudio.job.JobController;
+import cn.cnic.virostudio.job.ReStep;
+import cn.cnic.virostudio.job.Step;
 
 public class ReStartPathway {
-
 	private static Logger logerr = Logger.getLogger("errLog");
 	private static Logger loginfo = Logger.getLogger("infoLog");
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
 	public static void main(String[] args) throws Exception {
-//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-//				new String[] { "enzyme-job.xml" });
-//		JobController job = context.getBean("job", JobController.class);
-//		job.doListener();
-//		context.close();
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				new String[] { "pathway-job.xml" });
-		JobController job = context.getBean("job", JobController.class);
-		job.doListener();
+		ReStep step = context.getBean("restep", ReStep.class);
+		loginfo.info("pathway总条数是： "+step.doStep(0));
 		context.close();
 	}
 
