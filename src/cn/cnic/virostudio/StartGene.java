4d915da386c3bffbb9543b558f8d497d198218e9
luoluowushengmimi <luoluowushengmimi@gmail.com>
2014-11-15 09:25:53 +0800

    add gene step 3

--- src/cn/cnic/virostudio/StartGene.java
+++ src/cn/cnic/virostudio/StartGene.java
@@ -10,15 +10,20 @@ public class StartGene {
 	private static Logger logerr = Logger.getLogger("errLog");
 	private static Logger loginfo = Logger.getLogger("infoLog");
 	public static void main(String[] args) throws Exception {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[] { "gene-job-one.xml" });
-		Step step = context.getBean("step", Step.class);
-		loginfo.info("genome中包含gene 总条数是： "+step.doStep(0));
-		context.close();
-		ClassPathXmlApplicationContext con = new ClassPathXmlApplicationContext(
-				new String[] { "gene-job-two.xml"});
-		Step steptwo = con.getBean("step", Step.class);
-		loginfo.info("gene数据总条数是： "+steptwo.doStep(0));
-		con.close();
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//				new String[] { "gene-job-one.xml" });
+//		Step step = context.getBean("step", Step.class);
+//		loginfo.info("genome中包含gene 总条数是： "+step.doStep(0));
+//		context.close();
+//		ClassPathXmlApplicationContext con = new ClassPathXmlApplicationContext(
+//				new String[] { "gene-job-two.xml"});
+//		Step steptwo = con.getBean("step", Step.class);
+//		loginfo.info("gene数据总条数是： "+steptwo.doStep(0));
+//		con.close();
+		ClassPathXmlApplicationContext con2 = new ClassPathXmlApplicationContext(
+				new String[] { "gene-job-three.xml"});
+		Step step3 = con2.getBean("step", Step.class);
+		loginfo.info("gene数据总条数是： "+step3.doStep(0));
+		con2.close();
 	}
 }
